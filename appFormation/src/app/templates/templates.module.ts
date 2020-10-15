956b96a2d4beb951dfc028630dbfaee2fefde247
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-10-15 12:35:37 +0200

    Ajout du module global text

--- appFormation/src/app/templates/templates.module.ts
+++ appFormation/src/app/templates/templates.module.ts
@@ -5,13 +5,15 @@ import { TableDarkComponent } from './table-dark/table-dark.component';
 import { TableStripedComponent } from './table-striped/table-striped.component';
 import { TitreAComponent } from './titre-a/titre-a.component';
 import { TitreBComponent } from './titre-b/titre-b.component';
+import { TextsModule } from '../texts/texts.module';
 
 
 
 @NgModule({
   declarations: [TableLightComponent, TableDarkComponent, TableStripedComponent, TitreAComponent, TitreBComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TextsModule
   ], exports: [
     TableLightComponent,
     TableDarkComponent,
